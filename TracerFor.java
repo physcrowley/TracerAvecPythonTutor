61cf4c85e5a6bb8ba095ef316e8b5175780ccc9e
physcrowley <crowlda@ecolecatholique.ca>
2021-12-21 12:57:23 -0500

    contenu de TracerFor.java

--- TracerFor.java
+++ TracerFor.java
@@ -1,6 +1,25 @@
+/*
+    Dans Python Tutor, tracer le programme suivant en utilisant le bouton "Next >"
+  
+    1. Pourquoi pensez-vous que la boucle "for" est utilisée quand on connaît le nombre
+    exact de fois qu'on doit répéter une action (au lieu d'une boucle "while") ?
+ 
+ */
 public class TracerFor {
     public static void main(String[] args) {
+        int i = 0;
+
+        System.out.print("While :\n\t");
+        while ( i < 3 ) {
+            System.out.print( i + " " );
+            i++;
+        }
+        System.out.println("après : " + i );
         
+        System.out.print("For :\n\t");
+        for ( int j = 0; j < 3; j++ ){
+            System.out.print(j + " ");
+        }
+        // System.out.println( "après : " + j ); // ne fonctionne pas -> j est seulement défini dans le bloc for{}
     }
-    
 }
